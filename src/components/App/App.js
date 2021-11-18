c9d287eaa6dea22bc5532f66d77291cb314a6736
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2021-11-18 01:06:27 +0000

    Add a form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,12 @@
+import { ContactForm } from "../ContactForm";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ContactForm />
+    </div>
+  );
 }
 
 export { App };
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,83 @@
+import { useEffect } from "react";
+import { useForm, FormProvider } from "react-hook-form";
+import {
+  Box,
+  Button,
+  Flex,
+  Text,
+  TextField,
+  TextArea,
+} from "@simplybusiness/mobius-simplybusiness";
+import { ErrorMessage } from "@hookform/error-message";
+
+const Error = ({ name }) => (
+  <ErrorMessage
+    name={name}
+    render={({ message }) => (
+      <Text color="error" elementType="span">
+        {message}
+      </Text>
+    )}
+  />
+);
+
+function ContactForm() {
+  const formMethods = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(formMethods.formState.errors);
+  return (
+    <Box maxWidth="600px" margin="auto" padding="size-20">
+      <form onSubmit={formMethods.handleSubmit(onSubmit)}>
+        <FormProvider {...formMethods}>
+          <Flex gap="size-20" flexDirection="column">
+            <TextField
+              label="First name"
+              {...formMethods.register("firstName", {
+                required: "First name is required",
+              })}
+              onChange={(value) => formMethods.setValue("firstName", value)}
+              validationState={
+                formMethods.formState.errors.firstName && "invalid"
+              }
+            />
+            <Error name="firstName" />
+            <TextField
+              label="Last name"
+              {...formMethods.register("lastName", {
+                required: "Last name is required",
+              })}
+              onChange={(value) => formMethods.setValue("lastName", value)}
+              validationState={
+                formMethods.formState.errors.firstName && "invalid"
+              }
+            />
+            <Error name="lastName" />
+            {/* TODO: Rows doesn't work */}
+            {/* TODO: TextArea appears broken, error about refs */}
+            <TextArea
+              label="Message"
+              rows="10"
+              {...formMethods.register("message", {
+                required: "Message is required",
+              })}
+              onChange={(value) => formMethods.setValue("message", value)}
+              validationState={
+                formMethods.formState.errors.message && "invalid"
+              }
+            />
+            <Flex gap="size-20">
+              <Button type="submit" width="100%">
+                Send
+              </Button>
+              <Button type="reset" width="100%">
+                Reset
+              </Button>
+            </Flex>
+          </Flex>
+        </FormProvider>
+      </form>
+    </Box>
+  );
+}
+
+export { ContactForm };
