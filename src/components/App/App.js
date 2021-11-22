cf867c31f899dbc336eeff69399baae01b5b2634
Leslie Hoare <iam@lesleh.co.uk>
2021-11-22 14:07:12 +0000

    Remove formik form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,10 @@
 import { ContactForm } from "../ContactForm";
-import { ContactFormik } from "../ContactFormik";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>React Hook Form</h1>
       <ContactForm />
-      <h1>Formik</h1>
-      <ContactFormik />
     </div>
   );
 }
