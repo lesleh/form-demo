a1e712b366ecd96917d74ac8408c0f87c3e76f8a
Leslie Hoare <iam@lesleh.co.uk>
2021-11-22 14:07:24 +0000

    Add subject and yup validation

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useForm, FormProvider } from "react-hook-form";
 import {
   Box,
@@ -7,8 +8,58 @@ import {
   TextField,
   TextArea,
   Checkbox,
+  Select,
+  Option,
 } from "@simplybusiness/mobius-simplybusiness";
 import { ErrorMessage } from "@hookform/error-message";
+import * as yup from "yup";
+
+const useYupValidationResolver = (validationSchema) =>
+  useCallback(
+    async (data) => {
+      try {
+        const values = await validationSchema.validate(data, {
+          abortEarly: false,
+        });
+
+        return {
+          values,
+          errors: {},
+        };
+      } catch (errors) {
+        return {
+          values: {},
+          errors: errors.inner.reduce(
+            (allErrors, currentError) => ({
+              ...allErrors,
+              [currentError.path]: {
+                type: currentError.type ?? "validation",
+                message: currentError.message,
+              },
+            }),
+            {}
+          ),
+        };
+      }
+    },
+    [validationSchema]
+  );
+
+const validationSchema = yup.object({
+  firstName: yup.string().required("Required"),
+  lastName: yup.string().required("Required"),
+  email: yup.string().required("Required"),
+  subject: yup.string().required("Required"),
+  subjectOther: yup
+    .string()
+    .ensure()
+    .when("subject", {
+      is: (subject) => subject === "other",
+      then: yup.string().required("Required"),
+    }),
+  message: yup.string().required("Required"),
+  agree: yup.boolean().oneOf([true], "Required"),
+});
 
 const Error = ({ name }) => (
   <ErrorMessage
@@ -29,10 +80,14 @@ const validationState = (errors, name) => {
 };
 
 function ContactForm() {
+  const resolver = useYupValidationResolver(validationSchema);
   const formMethods = useForm({
+    resolver,
     reValidateMode: "onBlur",
   });
   const onSubmit = (data) => console.log(data);
+  const subject = formMethods.watch("subject");
+
   return (
     <Box maxWidth="600px" margin="auto" padding="size-20">
       <form onSubmit={formMethods.handleSubmit(onSubmit)}>
@@ -56,9 +111,7 @@ function ContactForm() {
               <Box flexBasis="100%">
                 <TextField
                   label="Last name"
-                  {...formMethods.register("lastName", {
-                    required: "Last name is required",
-                  })}
+                  {...formMethods.register("lastName")}
                   onChange={(value) => formMethods.setValue("lastName", value)}
                   validationState={validationState(
                     formMethods.formState.errors,
@@ -70,13 +123,7 @@ function ContactForm() {
             </Flex>
             <TextField
               label="Email address"
-              {...formMethods.register("email", {
-                required: "Email is required",
-                pattern: {
-                  value: /.@./,
-                  message: "Email is invalid",
-                },
-              })}
+              {...formMethods.register("email")}
               onChange={(value) => formMethods.setValue("email", value)}
               validationState={validationState(
                 formMethods.formState.errors,
@@ -84,6 +131,34 @@ function ContactForm() {
               )}
             />
             <Error name="email" />
+            <Select
+              label="Subject"
+              {...formMethods.register("subject")}
+              onChange={(value) => formMethods.setValue("subject", value)}
+              validationState={validationState(
+                formMethods.formState.errors,
+                "subject"
+              )}
+            >
+              <Option value="">Select a subject</Option>
+              <Option value="billing">Billing</Option>
+              <Option value="account">Account</Option>
+              <Option value="technical_support">Technical support</Option>
+              <Option value="other">Other</Option>
+            </Select>
+            <Error name="subject" />
+
+            <TextField
+              label="Other subject"
+              {...formMethods.register("subjectOther")}
+              onChange={(value) => formMethods.setValue("subjectOther", value)}
+              isDisabled={subject !== "other"}
+              validationState={validationState(
+                formMethods.formState.errors,
+                "subjectOther"
+              )}
+            />
+            <Error name="subjectOther" />
             {/* TODO: Rows doesn't work */}
             {/*
               Warning: Function components cannot be given refs. Attempts to
@@ -94,9 +169,7 @@ function ContactForm() {
             <TextArea
               label="Message"
               rows={100}
-              {...formMethods.register("message", {
-                required: "Message is required",
-              })}
+              {...formMethods.register("message")}
               onChange={(value) => formMethods.setValue("message", value)}
               validationState={
                 formMethods.formState.errors.message && "invalid"
@@ -105,9 +178,7 @@ function ContactForm() {
             <Error name="message" />
             <Checkbox
               label="I agree to the terms and conditions"
-              {...formMethods.register("agree", {
-                required: "You must agree to the terms and conditions",
-              })}
+              {...formMethods.register("agree")}
               onChange={(value) => formMethods.setValue("agree", value)}
               validationState={formMethods.formState.errors.agree && "invalid"}
             />
